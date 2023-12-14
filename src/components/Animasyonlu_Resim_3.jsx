9f634123875e6d4977b445036044c8840858757e
İsmail SARITEKE <95230928+sar1teke@users.noreply.github.com>
2023-12-14 14:33:50 +0300

    Update Animasyonlu_Resim_3.jsx

--- src/components/Animasyonlu_Resim_3.jsx
+++ src/components/Animasyonlu_Resim_3.jsx
@@ -14,23 +14,17 @@ const Animasyonlu_Resim_3 = () => {
       <div className="parallax-image" />
       <div className="content">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
-          voluptatem animi. Impedit tempore fugiat modi eius autem nihil
-          molestias neque. Laudantium sint quisquam ullam tempore corporis
-          voluptatem vero et fugiat! Commodi modi enim quidem sed exercitationem
-          ratione, voluptatibus sit? Odio dicta, veniam asperiores qui deleniti
-          velit impedit est atque commodi accusamus similique numquam ab
-          cupiditate suscipit ipsam consequuntur facere hic. Tempora natus velit
-          eum dignissimos totam porro quo recusandae esse! Magni, fugit officia?
-          Mollitia, culpa perspiciatis distinctio fugiat fugit est quis ut rerum
-          non magnam suscipit obcaecati quos, numquam laboriosam. Corporis
-          possimus sint molestiae dolor voluptas quas dolore ipsam magni sit
-          facere alias maxime animi incidunt veniam quo, debitis repudiandae
-          explicabo laboriosam eveniet odit soluta saepe! Iusto possimus harum
-          magni? Ab consectetur nisi doloremque vitae ut nesciunt expedita et
-          odio. Commodi error saepe eius modi quibusdam soluta aut, cupiditate
-          id quos. Officia neque in impedit dignissimos facere ex modi
-          perspiciatis!
+          Öncelikle merhabalar ben Selçuk Üniversitesi bilgisayar
+    mühendisliği  sınıf öğrencisi İsmail sarıteke öncelikle bu alanın neden seçtiğimi ve
+    bu alanda ne yapmak istediğimden kısaca bahsedeyim bilgisayarlar hep ilgimi çekmiştir
+    daha doğrusu teknolojinin bu kadar hızlı gelişmesi teknolojinin bu denli durdurulamaması
+    hep ilgimi çekmiştir hepimizin bildiği gibi teknolojinin bir sonu yok bu da benim ilgimi
+    çeken asıl nokta yani gittiği yere kadar gidiyor ben de onunla beraber gideyim nereye kadar
+    gidebilirim diye düşündüm o yüzden bu alanı seçtim bu alanda çalışmaya başladığımdan itibaren
+    özellikle bilgisayar mühendisliğine başlığımdan itibaren çeşitli dalları deneyimledim bunlardan
+    kısaca bahsedeyim siber güvenlik, mobil programlama,   veri işleme, görüntü işleme hepsiyle
+    tek tek ilgilendim ama şimdi Mega yazılım akademisinde katkılarıyla web alanında çalışıyorum
+    bu sitede bunun bir örneğidir aşağı kısımda da genel biyografimden bahsedeceğim.
         </p>
       </div>
     </motion.div>
