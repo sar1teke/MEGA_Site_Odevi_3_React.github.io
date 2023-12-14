f4feab75c6a51123f61220add72de6bc6b6d6154
İsmail SARITEKE <issariteke@hotmail.com>
2023-12-14 12:41:05 +0300

    update_5

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/deploying-vite-project-example/',
+  base:'/MEGA_Site_Odevi_3_React.github.io/',
   plugins: [react()],
 })
